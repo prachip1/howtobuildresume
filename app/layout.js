3954da46bbf1d7f2ee870451609c4ac273443e61
prachip1 <prachip1908@gmail.com>
2026-02-05 10:35:15 +0530

    adding analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -71,6 +72,7 @@ export default function RootLayout({ children }) {
           <main className="min-h-[calc(100vh-4rem)] flex-1 bg-ref-dark">{children}</main>
           <Footer />
         </div>
+        <Analytics />
       </body>
     </html>
   )
